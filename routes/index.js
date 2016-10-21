9b7d042f6aeb6a39748e0ea01897f9e26eaa1ea2
Aakash Tyagi <aakashtyagi@Aakashs-MacBook-Pro.local>
2016-10-21 16:35:21 -0500

    adding POST method to form in index.js

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,30 @@ router.get('/userlist', function(req,res){
     });
 });
 
+router.get('/newuser', function(req,res){
+	res.render('newuser', {title: 'Add new user'});
+});
+
+// POST to add user
+router.post('/adduser', function(req,res){
+	var db = req.db;
+
+	var username = req.body.username;
+	var email = req.body.useremail;
+
+	var collection = db.get('usercollection');
+
+	collection.insert({
+		"username": username,
+		"email": email
+	}, function(err,doc){
+		if (err){
+			res.send("There was a problem adding the information to the database.");
+		}
+		else{
+			res.redirect("userlist");
+		}
+	});
+});
+
 module.exports = router;
